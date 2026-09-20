105687c9a04efd94968164a873893d40cd28e99d
Hasaan-Khilji <hasaan.khilji@proton.me>
2026-09-20 16:47:27 +0500

    Added some comments

--- scripts/Player/character_body_2d.gd
+++ scripts/Player/character_body_2d.gd
@@ -24,20 +24,32 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 
-
 func _on_interaction_zone_area_entered(area: Area2D) -> void:
+	# check if area is interactable
 	if area is Interactable:
+		# if there was alredy an interabtable area in range hide its prompt
 		if interactable_in_range != null:
 			interactable_in_range.hide_prompt()
 		
+		 # show prompt of new interactable area
 		interactable_in_range = area as Interactable
 		interactable_in_range.show_prompt()
 
 func _on_interaction_zone_area_exited(area: Area2D) -> void:
+	# if area that exited was the latest one in range, then hide its prompt
 	if area == interactable_in_range:
 		interactable_in_range.hide_prompt()
 		interactable_in_range = null
 
+# Attack function
+func attack():
+	pass
+
+# Check for inputs
 func _input(event: InputEvent) -> void:
+	# If E is pressed, trigger interact function of interactable area
 	if event.is_action_pressed("Interact"):
 		interactable_in_range.interact()
+	# If attack key is pressed, then call the attack function
+	if event.is_action_pressed("Attack"):
+		attack();
--- scripts/system/interactable.gd
+++ scripts/system/interactable.gd
@@ -1,14 +1,26 @@
+# Attach this script to the Area2D of any interactable object
+# The parent node must have a script attached to it with a interact() function
+# The parent node must also have a label
+
+# This script handles the ineract function and showing/hiding the labels
+
 class_name Interactable
 extends Area2D
+
 @export var prompt_label: Label
+
+# Shows the text "[E]" in the prompt
 func show_prompt():
+	# make sure that the prompt is available
 	if prompt_label:
 		get_parent().label.text = "[E]"
 		get_parent().label.visible = true
-	
+
+# hides the prompt
 func hide_prompt():
 	if prompt_label:
 		get_parent().label.visible = false
 
+# triggers the parent's interact function
 func interact():
 	get_parent().interact()
