e2f276e03527f9156a2d25c0e525b7f98e733d23
Taha Faisal <digitalservicespakistan1@gmail.com>
2026-09-21 02:01:58 +0500

    pond added

--- scripts/system/HUD.gd
+++ scripts/system/HUD.gd
@@ -10,7 +10,6 @@ func _ready() -> void:
 	Inventory.inventory_changed.connect(_on_inventory_changed)
 	_on_inventory_changed()
 	
-	
 func _on_inventory_changed() -> void:
 	label_health.text = "❤️  " + str(Inventory.health)
 	label_seeds.text  = "🌱  " + str(Inventory.seeds)
