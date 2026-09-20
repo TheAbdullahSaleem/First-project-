ff2c6bffdf52862508fae9fa4ee9adb24a524a23
Taha Faisal <digitalservicespakistan1@gmail.com>
2026-09-20 21:41:14 +0500

    plis fix character

--- scripts/Player/character_body_2d.gd
+++ scripts/Player/character_body_2d.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
-var i_eat = "True"
+
 var interactable_in_range = null
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+@onready var anim = $AnimatedSprite2D
 
 func _ready() -> void:
 	if not $Area2D.area_entered.is_connected(_on_area_2d_area_entered):
@@ -12,36 +13,32 @@ func _ready() -> void:
 		$Area2D.area_exited.connect(_on_area_2d_area_exited)
 
 func _physics_process(delta: float) -> void:
-	velocity += get_gravity() * delta
-	
-	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
+	_update_animation(direction)
+
+func _update_animation(direction: float) -> void:
+	if direction > 0:
+		anim.play("right")
+	elif direction < 0:
+		anim.play("left")
+	else:
+		anim.play("stand")
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	# check if area is interactable
 	if area is Interactable:
-		# if there was alredy an interabtable area in range hide its prompt
 		if interactable_in_range != null:
 			interactable_in_range.hide_prompt()
-		
-		 # show prompt of new interactable area
 		interactable_in_range = area as Interactable
 		interactable_in_range.show_prompt()
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
-	# if area that exited was the latest one in range, then hide its prompt
 	if area == interactable_in_range:
 		interactable_in_range.hide_prompt()
 		interactable_in_range = null
@@ -52,11 +49,7 @@ func attack():
 
 # Check for inputs
 func _input(event: InputEvent) -> void:
-	# If E is pressed, trigger interact function of interactable area
-
 	if event.is_action_pressed("Interact") and interactable_in_range != null:
-		interactable_in_range.interact()  # null reference error
-
-	# If attack key is pressed, then call the attack function
+		interactable_in_range.interact()
 	if event.is_action_pressed("Attack"):
-		attack();
+		attack()
